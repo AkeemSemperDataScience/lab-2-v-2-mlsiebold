6fc97170e22e9357717c783420fc56fd258b9659
mlsiebold <m.siebold@live.com>
2024-09-23 15:07:48 -0600

    Attemp 1B

--- START_Lab_2.py
+++ START_Lab_2.py
@@ -2,26 +2,61 @@ def lab2Question1(word):
     # Note - you'll need to change the signature (above) to match the arguments for this lab...
     # Create a function that takes in a string 
     # Return True if that string is a palindrome, False otherwise
-    pass
+    
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+    
 
 def lab2Question2(number_val):
     # Create a function that takes in a number
     # Return a list of the fibonacci sequence up to that number
-    pass
+    
+    num1 = 0
+    num2 = 1
+    
+    while num2 <= number_val:
+        print(num1)
+        new_num = num1 + num2
+        num1 = num2   
+        num2 = new_num
+        
+        if num2 > number_val:
+            break
+        return num1
+
 
 def lab2Question3(str1, str2):
     # Create a function that takes in two strings - str1 and str2
     # Return the number of times str2 appears in str1
     # For example if str1 = "coding is cool" and str2 = "co" then output should be 2.
-    pass
+    
+    str1 = str[]
+    str2 = str[]
+
+    strcount = str1.count(str2)
+
+    return strcount
+
 
 def lab2Question4(list1, list2):
     # Create a function that takes in two equal length list of numbers. 
     # Return a list of the element-wise sum of the two lists - i.e. the first element of the output list is the sum of the first elements of the input lists
     # If the condition of the function is not satisfied, return a blank list
-    pass
+    
+    sum_list = []   
+    
+    if len(list1) == len(list2):
+        list3 = zip(list1, list2)
+        zip_list = list(list3)
+        
+        for i in range(0,len(zip_list)):
+            sum_list.append(list1[i] + list2[i])
+        return sum_list
+    else:
+        return []
 
-    return sum_list
 
 def lab2Question5():
     # Create a function* that asks a user to enter a password that meets the following criteria:
